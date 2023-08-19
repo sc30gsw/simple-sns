b57ccaca4a912fd38aaa1a723ae7e69345c72461
sc30gsw <haidoushi875@gmail.com>
2023-08-20 07:40:36 +0900

    ログインAPIの作成

--- api/src/server.ts
+++ api/src/server.ts
@@ -7,6 +7,8 @@ import {
   validUsernameExistsAndLength,
 } from './services/validation/userValid'
 import { printErrors } from './services/validation/validation'
+import 'dotenv/config'
+import jwt from 'jsonwebtoken'
 
 const app = express()
 
@@ -24,30 +26,63 @@ app.post(
   validPasswordLength,
   printErrors,
   async (req: express.Request, res: express.Response) => {
-    const { username, email, password } = req.body
+    try {
+      const { username, email, password } = req.body
 
-    const foundUser = await prisma.user.findUnique({ where: { email: email } })
-    if (foundUser)
-      return res
-        .status(409)
-        .json({ msg: 'メールアドレスはすでに登録されています' })
+      const foundUser = await prisma.user.findUnique({ where: { email } })
+      if (foundUser)
+        return res
+          .status(409)
+          .json({ msg: 'メールアドレスはすでに登録されています' })
 
-    const hashedPassword = await bcrypt.hash(password, 10)
+      const hashedPassword = await bcrypt.hash(password, 10)
 
-    const user = await prisma.user.create({
-      data: {
-        username,
-        email,
-        password: hashedPassword,
-      },
-    })
+      const user = await prisma.user.create({
+        data: {
+          username,
+          email,
+          password: hashedPassword,
+        },
+      })
 
-    return res.status(201).json({ user })
+      return res.status(201).json({ user })
+    } catch (err) {
+      return res.status(500).json(err)
+    }
   },
 )
 
 // ユーザーログインAPI
-app.post('/api/auth/login', validEmailExistsAndFormat, validPasswordLength)
+app.post(
+  '/api/auth/login',
+  validEmailExistsAndFormat,
+  validPasswordLength,
+  printErrors,
+  async (req: express.Request, res: express.Response) => {
+    try {
+      const { email, password } = req.body
+
+      const user = await prisma.user.findUnique({ where: { email } })
+
+      if (!user) return res.status(401).json({ msg: 'ユーザーが存在しません' })
+
+      const isPasswordValid = await bcrypt.compare(password, user.password)
+
+      if (!isPasswordValid)
+        return res.status(401).json({ msg: 'そのパスワードは間違っています' })
+
+      const token = jwt.sign(
+        { id: user.id },
+        process.env.TOKEN_SECRET_KEY as string,
+        { expiresIn: '24h' },
+      )
+
+      return res.status(201).json({ token })
+    } catch (err) {
+      return res.status(500).json(err)
+    }
+  },
+)
 
 app.listen(PORT, () => {
   console.log(`server is running on Port ${PORT}`)
