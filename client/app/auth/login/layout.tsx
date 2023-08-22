f7aa771433fb307008c473f4edbae54916762ff4
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-08-22 13:04:20 +0900

    childrenに書き換え

--- client/app/auth/login/layout.tsx
+++ client/app/auth/login/layout.tsx
@@ -1,15 +1,13 @@
 import type { Metadata } from 'next'
 import React from 'react'
 
-import Login from './page'
-
 export const metadata: Metadata = {
   title: 'ログイン',
   description: 'login page',
 }
 
 const LoginLayout = ({ children }: { children: React.ReactNode }) => {
-  return <Login />
+  return <>{children}</>
 }
 
 export default LoginLayout
